b96923e7ae2e851283b052e0284ea4775085192e
Ayushi <ayushimishra361@gmail.com>
2024-06-25 23:58:00 +0530

    Added Docx code and chunk process

---
+++ chunks.py
@@ -0,0 +1,11 @@
+from langchain_text_splitters import CharacterTextSplitter
+from langchain.docstore.document import Document
+
+def split_with_character_text_splitter(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    return text_splitter.split_text(text)
